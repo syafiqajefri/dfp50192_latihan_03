f5ff78bc8088b5cde7cc36837e060623ff5889d5
syafiqajefri <syafiqajefri1705@gmail.com>
2021-10-04 11:56:27 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 03</title>
+</head>
+<body>
+    <h1>Asas PHP</h1>
+    
+    <?php 
+    //komen satu baris
+    #komen satu baris
+    /* 
+        komen banyak baris
+    */
+
+    $namapelajar = 'Nur Syafiqa Syazwani Jefri';
+    echo '<h2>Hello $namapelajar </h2>'; #output
+
+    $hadir = false;
+    $umur = 20;
+    $nokp = '010505090048';
+
+    ?>
+</body>
+</html>
